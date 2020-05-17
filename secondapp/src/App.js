1999643343edceab423245d57610bc6a660b0c17
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-05-16 20:51:49 -0400

    commit

--- secondapp/src/App.js
+++ secondapp/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import './App.css';
+import Namebar from'./Components/Namebar'
+import Button from './Components/Button'
 
-function App() {
-  const list = [1,2,3,4,5,6];
+class App extends React.Component{
 
-  const data = num => {
+ render(){
+    return (
+      <div className='box'>
+       <div className='line'>
+        <Namebar className='bar' name='Smit'/>
+        <Button className='btn'/>
+       </div>
 
-    if (num === 40)
-     return <h1>{num}</h1>
+       <Content className='content' content='ABC'>
+      </div>
+    )
+ }
 
-    return <li>{num}</li>
-  }
-
-  return (
-    <div className="App">
-      {list.map( num=> data(num*10) )}
-    </div>
-  );
 }
 
 export default App;
---
+++ secondapp/src/Components/Namebar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Namebar(props){
+  return <div className={props.className}>{props.name}</div>
+}
+
+export default Namebar;
